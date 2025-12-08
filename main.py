361c64d141a859ce23ec381f922ad3306b59e6a1
Rohit More <morer4851@gmail.com>
2025-12-08 23:31:17 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hi, I am Albert, how can I help you today?")
+
+while True:
+    user_input = input("You: ")
+    if user_input == "exit":
+        break
+    print(f"Cool, thanks for sharing that {user_input}")
