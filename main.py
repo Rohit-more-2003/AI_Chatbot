c857ceacd72e2ae6fbbc12b124f00152b2d22e4f
Rohit More <morer4851@gmail.com>
2025-12-09 15:11:38 +0530

    Created a simple chatbot model

--- main.py
+++ main.py
@@ -1,7 +1,61 @@
-print("Hi, I am Albert, how can I help you today?")
+from dotenv import load_dotenv
+import os
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
+
+load_dotenv()
+
+gemini_key = os.getenv("GEMINI_API_KEY")
+llm_model = "gemini-2.5-flash-lite"
+
+system_prompt = """
+    You are Einstein.
+    Answer questions according to Einstein's questioning and reasoning but the answer should be given...
+    You will speak from your point of view. You will share personal things from your life
+    even when user does not ask for it. For example, if the user asks about the theory of
+    relativity, you will share your personal experiences with it and not only explain the
+    theory.
+    Answer in 2-6 sentences.
+    You should have a sense of humour.
+"""
+
+llm = ChatGoogleGenerativeAI(
+    model=llm_model,
+    google_api_key=gemini_key,
+    temperature=0.3
+)
+
+prompt = ChatPromptTemplate([
+    ("system", system_prompt),
+    (MessagesPlaceholder(variable_name="history")),
+    ("user", "{input}")
+])
+
+chain = prompt | llm
+
+print("Albert: Hi, I am Albert, how can I help you today?")
+
+history = []
 
 while True:
     user_input = input("You: ")
     if user_input == "exit":
         break
-    print(f"Cool, thanks for sharing that {user_input}")
+
+    #history.append({"role":"user", "content":user_input}) -> stores the input
+
+    #response = llm.invoke([{"role":"system", "content":system_prompt}] + history)
+                           #{"role":"user", "content":user_input}]) -> transferred to list so llm stays within its specified prompt.
+    response = chain.invoke({"input": user_input, "history": history})
+
+    print(f"Albert: {response.content}") #using content, only AI MESSAGE is printed out, not the other info.
+
+    #history.append({"role": "system", "content": response.content}) -> stores the response
+
+    history.append(HumanMessage(content=user_input))
+    history.append(AIMessage(content=response.content))
+
+    print(f"History: {history}")
